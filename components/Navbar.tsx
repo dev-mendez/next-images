8e39b19bd627351ef77eef16668fa8216dbb8310
An2 <an2m001@gmail.com>
2023-06-11 11:37:44 -0400

    update new

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+
+function Navbar() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  )
+}
+
+Navbar.propTypes = {
+
+}
+
+export default Navbar
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Navbar from '../../components/Navbar'
 
 const getUsers = async () => {
   const res = await fetch('https://reqres.in/api/users?page=2')
@@ -10,16 +11,19 @@ const getUsers = async () => {
 export default async function Home() {
   const users = await getUsers()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul className='grid grid-flow-col gap-4'>
-        {users.map((user: any) => (
-          <Link key={user.id} href={`${user.id}`}>
-            <li className=' hover:cursor-pointer' key={user.id}>
-              <Image alt='Image' width={100} height={100} src={user.avatar} />
-              <h3>{user.first_name}</h3>
-            </li>
-          </Link>))}
-      </ul>
-    </main>
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <ul className='grid grid-flow-col gap-4'>
+          {users.map((user: any) => (
+            <Link key={user.id} href={`${user.id}`}>
+              <li className=' hover:cursor-pointer' key={user.id}>
+                <Image alt='Image' width={100} height={100} src={user.avatar} />
+                <h3>{user.first_name}</h3>
+              </li>
+            </Link>))}
+        </ul>
+      </main>
+    </>
   )
 }   
